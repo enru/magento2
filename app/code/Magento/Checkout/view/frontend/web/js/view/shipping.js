2c1d0768306939e60ae4149facf645b5d3d90f94
Neill Russell <git@enru.co.uk>
2019-02-07 18:38:17 +0000

    fix: make save in address book dynamic
    addressData['save_in_address_book'] was being set to static value this.saveInAddressBook
    so ALL shipping address are saved to address book regardless of form data

--- app/code/Magento/Checkout/view/frontend/web/js/view/shipping.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/shipping.js
@@ -208,7 +208,7 @@ define([
             if (!this.source.get('params.invalid')) {
                 addressData = this.source.get('shippingAddress');
                 // if user clicked the checkbox, its value is true or false. Need to convert.
-                addressData['save_in_address_book'] = this.saveInAddressBook ? 1 : 0;
+                addressData['save_in_address_book'] = addressData['save_in_address_book'] ? 1 : 0;
 
                 // New address must be selected as a shipping address
                 newShippingAddress = createShippingAddress(addressData);
